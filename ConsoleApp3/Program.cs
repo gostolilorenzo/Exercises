567d7a910ab7ae25d0cfb778a5b4a0f9076f237b
Lorenzo <gostolilorenzo17@gmail.com>
2023-06-01 00:35:23 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,11 @@
+﻿string numeri = Console.ReadLine();
+int cont = 0; 
+foreach (char c in numeri)
+{
+    if (c == 'a' ||c == 'A') 
+    {
+        cont++;   
+    }
+    
+}
+Console.WriteLine($"Trovate { cont} a/A");
