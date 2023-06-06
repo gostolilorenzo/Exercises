1085f69d1eee6d0607cb2b5127a0cbdea2dd149f
Lorenzo <gostolilorenzo17@gmail.com>
2023-06-07 00:34:38 +0200

    numeri min/max

--- Arrey1/Program.cs
+++ Arrey1/Program.cs
@@ -40,6 +40,39 @@ namespace array_1
             string[] colori = { "giallo", "verde", "rosso" };
             Console.WriteLine(colori[1]);
 
+            //scegli il primo numero e confrontalo con gli altri se c'è un numero più piccolo usa quello per il confronto
+
+            int min_num = 0;
+
+            for (int i = 0; i<max_numeri; i++) //faccio il codice seguente 50 volte
+            {
+                if (numeri_casuali[i] < numeri_casuali[min_num])
+                {
+                    min_num = i;
+                }
+                
+                //if (numeri_casuali[i] < numeri_casuali[i + 1];
+            }
+            Console.WriteLine(numeri_casuali[min_num]);
+
+
+            int max_num = 0;
+            for (int i = 0; i < max_numeri; i++) //faccio il codice seguente 50 volte
+            {
+                if (numeri_casuali[i] > numeri_casuali[max_num])
+                {
+                    max_num = i;
+                }
+
+                //if (numeri_casuali[i] < numeri_casuali[i + 1];
+            }
+            Console.WriteLine(numeri_casuali[max_num]);
+
+            //ARRAY
+            //nomeArray[posizione]
+
+
+            //if (numeri_casuali[0] < numeri_casuali[1])
 
         }
     }
