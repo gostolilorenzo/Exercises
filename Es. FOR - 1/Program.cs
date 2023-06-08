1aaead0a74b21d82c004ed8f85f4ae68112dc201
Lorenzo <gostolilorenzo17@gmail.com>
2023-06-08 17:52:51 +0200

    fix nome "Cicli"

---
+++ Es. FOR - 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Es.Cicli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            
+            while (n < 501);
+            {
+                Console.WriteLine($"{n} ");
+                n++;
+            }
+
+        }
+    }
+}
