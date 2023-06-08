09adfd56496dca819373b4f9add3720db9f13796
Lorenzo <gostolilorenzo17@gmail.com>
2023-06-09 01:07:59 +0200

    ultimo esercizio Somma dei primi 100 numeri primi

--- Es. FOR - 1/Program.cs
+++ Es. FOR - 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata;
 
 namespace Es.Cicli
 {
@@ -6,13 +7,65 @@ namespace Es.Cicli
     {
         static void Main(string[] args)
         {
-            int n = 1;
-            
-            while (n < 501);
+
+            //STAMPA 500 NUMERI
+
+            //int n = 1;
+
+            //while (n <= 500)
+            //{
+            //    Console.WriteLine($"{n} ");
+            //    n++;
+            //}
+            //Console.WriteLine("Premi un tasto per continuare ...");
+            //Console.ReadKey();
+
+            //int n = 1;      
+            //int trovati = 0;
+            //int numeroBello = 7;
+            //int numeroCheAumenta = 1;
+
+
+            //Console.WriteLine(n % 2 != 0 ? : n++);
+
+
+            //PRIMI 100 NUMERI DISPARI
+
+            //do
+            //{
+            //    if (n % 2 != 0)
+            //    {
+            //        console.writeline(n);
+            //        trovati++;
+            //    }
+            //    n++;
+            //}
+            //while (trovati < 100);           
+
+
+            //MULTIPLI DI 7
+
+            //do
+            //{
+            //    n = numeroBello * trovati;
+            //    Console.WriteLine(n);
+            //    trovati++;               
+            //}
+
+            //while (trovati <= 50);
+
+
+            //SOMMA DEI PRIMI 100 NUMERI INTERI
+
+            int n = 0;
+            int trovati = 0;
+
+            do
             {
-                Console.WriteLine($"{n} ");
-                n++;
+                n = n + trovati;                
+                trovati++;
             }
+            while (trovati <= 100); Console.WriteLine($"SOMMA DEI PRIMI 100 NUMERI INTERI: {n }");
 
         }
     }
