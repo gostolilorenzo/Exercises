e11df3f9cc1f81f39917d35e97552aa65538ccbc
Lorenzo <gostolilorenzo17@gmail.com>
2023-11-06 11:59:49 +0100

    conclusione switch

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -45,31 +45,35 @@ namespace switch_test
 
             //} while (colore != "nero");
 
-            CultureInfo[] cultures = CultureInfo.GetCultureInfo(CultureTypes.AllCultures);
+            //CultureInfo[] cultures = CultureInfo.GetCultureInfo(CultureTypes.AllCultures);
 
-            CultureInfo[] cultures = new CultureInfo("En-en");
+            //CultureInfo[] cultures = new CultureInfo("En-en");
 
-            Console.WriteLine("Inserisci un numero con la virgola\n");
-            double.TryParse(Console.ReadLine(), NumberStyles.Float | NumberStyles.AllowThousands,
-                            culture, out double d);
-            switch (d)
-            {
-                case 3.14:
-                    Console.WriteLine("Il famoso PI greco!");
-                    break;
+            //Console.WriteLine("Inserisci un numero con la virgola\n");
+            //double.TryParse(Console.ReadLine(), NumberStyles.Float | NumberStyles.AllowThousands,
+            //                culture, out double d);
+            //switch (d)
+            //{
+            //    case 3.14:
+            //        Console.WriteLine("Il famoso PI greco!");
+            //        break;
 
-                default:
-                    Console.WriteLine("un double come un altro...");
-                    break;
-            }
+            //    default:
+            //        Console.WriteLine("un double come un altro...");
+            //        break;
+            //}
 
-            Console.WriteLine(d.ToString(culture));
+            //Console.WriteLine(d.ToString(culture));
+
+
+
+
             //int eta = 23; int stipendio = 4000;
             //string colore = "ciao";
             //double d = 3.14;
             //long l = 345;
 
-            //object x = eta;
+            //object x = l;
 
             //switch (x)
             //{
@@ -78,37 +82,43 @@ namespace switch_test
             //        break;
 
             //    case double num_double:
-            //        Console.WriteLine($"Elaboro un intero: {num_double}");
+            //        Console.WriteLine($"Elaboro un double: {num_double}");
             //        break;
 
             //    case string stringa:
             //        Console.WriteLine($"Elaboro una stringa: {stringa}");
             //        break;
 
-            //    case default:
-            //        Console.WriteLine($"Tipo di dato non previsto: {x.ToString()}");
+            //    default:
+            //        Console.WriteLine($"Tipo di dato non previsto: {x.ToString() }");
             //        break;
             //}
 
-            //int giorno = 3;
-            //string giorno_settimana = giorno switch
-            //{
-            //    1 => "Lunedì",
-            //    2 => "Martedì",
-            //    3 => "Mercoledì",
-            //    _ => "Non è un giorno della settimana correttamente codificato"
-            //};
 
-            //Console.WriteLine(giorno_settimana);
 
-            //Console.WriteLine(giorno switch
-            //    {
-            //        1 => "Lunedì",
-            //        2 => "Martedì",
-            //        3 => "Mercoledì",
-            //        _ => "Non è un giorno della settimana correttamente codificato"
-            //    }
-            //);
+
+            int giorno = 3;
+            string giorno_settimana = giorno switch
+            {
+                1 => "Lunedì",
+                2 => "Martedì",
+                3 => "Mercoledì",
+                _ => "Non è un giorno della settimana correttamente codificato"
+            };
+            
+            // => questo simbolo sta per "freccia"
+            // _ questo simbolo sta per ""default"
+
+            Console.WriteLine(giorno_settimana);
+
+            Console.WriteLine(giorno switch
+            {
+                1 => "Lunedì",
+                2 => "Martedì",
+                3 => "Mercoledì",
+                _ => "Non è un giorno della settimana correttamente codificato"
+            }
+            );
 
         }
     }
